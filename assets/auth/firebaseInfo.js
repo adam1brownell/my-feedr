b5745cda928bac95bb7a2ce6433054522851d47a
adam1brownell <adam1brownell@gmail.com>
2018-05-18 12:45:13 -0700

    remove firebase info

--- assets/auth/firebaseInfo.js
+++
@@ -1,8 +0,0 @@
-export var config = {
-  apiKey: "AIzaSyD2SGSt8Bj0QD0gxuhtKKEyGeMbfaSlEyY",
-  authDomain: "feedr-36a55.firebaseapp.com",
-  databaseURL: "https://feedr-36a55.firebaseio.com",
-  projectId: "feedr-36a55",
-  storageBucket: "feedr-36a55.appspot.com",
-  messagingSenderId: "184026495188"
-};
